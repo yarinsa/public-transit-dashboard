aaa3fa93f9160b6342701a9d587eeaf2c6b2a37a
Yarin Sasson <yarin@linxsecurity.io>
2025-03-05 20:24:48 +0200

    customize theme

---
+++ src/components/ui/theme.ts
@@ -0,0 +1,21 @@
+import { createSystem,defaultConfig,defineConfig } from "@chakra-ui/react"
+
+const customConfig = defineConfig({
+  theme: {
+    semanticTokens: {
+      colors: {
+        bg: {
+          DEFAULT: {
+            value: { _light: "{colors.gray.50}", _dark: "{colors.gray.950}" },
+          },
+          subtle: {
+            value: { _light: "{colors.gray.100}", _dark: "{colors.gray.900}" },
+          },
+        },
+      },
+    },
+  },
+});
+
+export const system = createSystem(defaultConfig, customConfig)
+console.log(system)
